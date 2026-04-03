59c54acabb58a594ae08adade53233c862b69105
cwgzjilongpo <181658877@qq.com>
2026-04-03 14:22:05 +0800

    fix: update location in personal introduction from Bengaluru, India to Malaysia

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -8,7 +8,7 @@
     </h1>
     <p class="text-gray-900 dark:text-gray-400">
       I'm Thomas, I work as a software, product engineer and designer from
-      Bengaluru, India. I specialize in building web applications and sites
+      Malaysia. I specialize in building web applications and sites
       using Javascript, React, Vue &amp; Node. I've procrastinated building this
       website for years but finally it's here, I've carved out my own little
       nook on the internet to share my silly experiments, nifty projects, and
@@ -16,7 +16,7 @@
     </p>
     <p class="text-gray-900 dark:text-gray-400">
       By day, I'm a Fullstack Developer at
-      <a href="https://headshotpro.com" target="_blank" class="underline">Headshotpro</a>, and
+      <a href="https://www.tapgoing.com/" target="_blank" class="underline">Tap Go</a>, and
       by night (and weekends), I'm busy tinkering with some random tool or app
       that I am building.
     </p>
@@ -27,6 +27,6 @@
 useSeoMeta({
   title: "Thomas Gwee",
   description:
-    "I'm Thomas, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
+    "I'm Thomas, your friendly neighborhood software, product engineer and designer from Malaysia. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
 });
 </script>
